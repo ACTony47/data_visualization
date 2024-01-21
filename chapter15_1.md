b842c31e358a833c85dc5d0a3cc57574fc7ca5a4
ACTony47 <68453250+ACTony47@users.noreply.github.com>
2024-01-21 11:18:49 +0800

    折线图的样式设置

--- chapter15_1.md
+++ chapter15_1.md
@@ -1,2 +1,3 @@
-1. `fig, ax = plt.subplots()` fig是图形对象，ax是轴对象 
+1. `fig, ax = plt.subplots()` fig是生成的整个图形，ax表示图形中的绘图
 2. `ax.plot(squares)` 只提供了一个列表，默认为y轴 x轴为**0，1，2，3，4**
+3. 使用`plt.style.use('seaborn-v0_8') ` 设置style 可以用plt.style.available查看可用的style
--- mpl_squares.py
+++ mpl_squares.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
+input_nums = [1, 2, 3, 4, 5]
+plt.style.use('seaborn-v0_8')  # set the style of the plot)
 fig, ax = plt.subplots()  # fig and ax are the figure and axis objects
-ax.plot(squares)  # plot the squares list
+
+ax.plot(input_nums, squares, linewidth=3)  # plot the squares list
+ax.set_title("square numbers", fontsize=23)  # set the title and font size
+ax.set_xlabel("index", fontsize=14)  # set the x-axis label and font size
+ax.set_ylabel("square", fontsize=14)  # set the y-axis label and font size
+ax.tick_params(axis='both', labelsize=14) # 对两个轴都进行设置， 刻度标签大小为14
 plt.show()
